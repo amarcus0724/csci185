c18305272e610cda4eff8f457e33c3dfe6e0eb4c
Abby <amarcus@unca.edu>
2023-04-28 12:12:34 -0400

    final exam

---
+++ final-exam/javascript-exercises/exercise01/main.js
@@ -0,0 +1,25 @@
+function color1(bgcolor) {
+    document.querySelector('#square1').style.backgroundColor = bgcolor
+
+    
+}
+
+function color2(bgcolor) {
+    document.querySelector('#square2').style.backgroundColor = bgcolor
+}
+
+function color3(bgcolor) {
+    document.querySelector('#square3').style.backgroundColor = bgcolor
+}
+
+function color4(bgcolor) {
+    document.querySelector('#square4').style.backgroundColor = bgcolor
+}
+
+function color5(bgcolor) {
+    document.querySelector('#square5').style.backgroundColor = bgcolor
+}
+
+function color6(bgcolor) {
+    document.querySelector('#square6').style.backgroundColor = bgcolor
+}
