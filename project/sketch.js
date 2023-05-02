c5be47f8ebb3b437dbe25145eced56dce4cf63f5
Abby <amarcus@unca.edu>
2023-05-02 16:58:51 -0400

    final project turn in

--- project/sketch.js
+++ project/sketch.js
@@ -7,13 +7,13 @@ const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight; 
 const pellets = [
    {x: 600, y: 650, width:20}, 
-   {x: 200, y: 150, width:20},
+   {x: 200, y: 155, width:20},
    {x: 175, y: 260, width:20},
    {x: 50, y: 700, width:20},
    {x: 700, y: 350, width:20},
    {x: 550, y: 200, width:20},
-   {x: 1200, y: 100, width:20},
-   {x: 850, y: 100, width:20},
+   {x: 1200, y: 120, width:20},
+   {x: 850, y: 230, width:20},
    {x: 1000, y: 600, width:20},
    {x: 1350, y: 690, width:20},
 
@@ -88,7 +88,9 @@ function moveController(ev) {
 function showMessage() {
    textSize(40); 
     fill('#062c43');
-   text('Collect the dots using the arrow keys!', 400, 60);
+   // text('Collect the dots using the arrow keys!', 400, 60);
+   
+   
 }
 function drawPellets() {
    for (let i = 0; i < pellets.length; i++) {
